86690598467b74ab3abed2ae859329377f2b100a
Alyssa Dai <alyssa.ydai@gmail.com>
2023-04-19 22:50:13 -0400

    configure GitHub workflow for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,18 @@
 from pathlib import Path
 
 import pytest
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_setup_options():
+    """
+    Ensure dash tests run in headless mode to run properly with GitHub actions.
+    (Ref: https://community.plotly.com/t/dash-integration-testing-with-selenium-and-github-actions/43602)
+    """
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    return options
 
 
 @pytest.fixture(scope="session")
