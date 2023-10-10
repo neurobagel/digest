aa8871a5750f3f73ead3e6162ef85368f1ca4770
Alyssa Dai <alyssa.ydai@gmail.com>
2023-10-09 23:32:37 -0400

    test reset_column_dtypes()

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 import pytest
 
 import proc_dash.utility as util
@@ -15,3 +17,27 @@ def test_invalid_filetype_returns_informative_error(filename):
 
     assert bagel is None
     assert "Invalid file type" in upload_error
+
+
+def test_reset_column_dtypes():
+    """
+    Test that reset_column_dtypes() infers more appropriate dtypes for columns whose values were erroneously stored as strings,
+    and that the 'session' column is always converted to strings (object dtype).
+    """
+    pheno_overview_df = pd.DataFrame(
+        {
+            "participant_id": ["sub-1", "sub-2", "sub-3"],
+            "session": [1, 1, 1],
+            "group": ["PD", "PD", "PD"],
+            "moca_total": ["21", "24", np.nan],
+            "moca_total_status": ["true", "true", "false"],
+        }
+    )
+
+    pheno_overview_df_retyped = util.reset_column_dtypes(pheno_overview_df)
+
+    assert pheno_overview_df_retyped["participant_id"].dtype == "object"
+    assert pheno_overview_df_retyped["session"].dtype == "object"
+    assert pheno_overview_df_retyped["group"].dtype == "object"
+    assert pheno_overview_df_retyped["moca_total"].dtype == "float64"
+    assert pheno_overview_df_retyped["moca_total_status"].dtype == "bool"
