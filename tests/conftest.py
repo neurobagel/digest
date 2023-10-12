3407eee7f10458b9dc7a934923f8fbcd803fa176
Alyssa Dai <alyssa.ydai@gmail.com>
2023-10-12 10:01:00 -0400

    update browser options so all tested components are visible

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,8 +10,13 @@ def pytest_setup_options():
     (Ref: https://community.plotly.com/t/dash-integration-testing-with-selenium-and-github-actions/43602)
     """
     options = Options()
-    options.add_argument("--headless")
+    # See https://www.selenium.dev/blog/2023/headless-is-going-away/#after for the renewed headless mode
+    options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
+
+    # Set a large window size and (to be extra safe) rescale browser to ensure tested components are all visible and can be interacted with without scrolling
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument("force-device-scale-factor=0.30")
     return options
 
 
