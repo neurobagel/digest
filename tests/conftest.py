a1536da63af78ef709dba4cbcb877f32a397a4be
Alyssa Dai <alyssa.ydai@gmail.com>
2024-03-07 12:37:04 -0500

    [MNT] Add `chromedrive-autoinstaller` for easier local testing (#145)
    * add chromedrive-autoinstaller for easier local testing
    
    * remove manual chromedriver installation steps from README

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import chromedriver_autoinstaller
 import pytest
 from selenium.webdriver.chrome.options import Options
 
@@ -9,6 +10,11 @@ def pytest_setup_options():
     Ensure dash tests run in headless mode to run properly with GitHub actions.
     (Ref: https://community.plotly.com/t/dash-integration-testing-with-selenium-and-github-actions/43602)
     """
+    # Automatically install the chromedriver that matches the currently installed Chrome version on the system if it's not already installed
+    # Mostly useful for running tests locally (to avoid having to update the chromedriver whenever there's a browser update),
+    # as GitHub actions should already have a compatible chromedriver installed
+    chromedriver_autoinstaller.install()
+
     options = Options()
     # See https://www.selenium.dev/blog/2023/headless-is-going-away/#after for the renewed headless mode. This is needed for the window rescaling to work properly.
     options.add_argument("--headless=new")
